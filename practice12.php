6c4a7fc58dd756e8660fcd18c0fd3e4f6e4169f7
yoshida <hcaeerr.221@gmail.com>
2021-01-30 04:37:35 +0000

    課題修正

--- practice12.php
+++ practice12.php
@@ -1,17 +1,19 @@
 <?php
 echo 'hello php!';
-$name = yoshida;
-if ($name = yoshida) {
+$name = 'yoshida';
+if ($name === 'yoshida') {
   echo "私は yoshida です";
 } else {
   echo "あなたの名前ではありません";
 }
 $total = 0;
 echo $total;
-for ($i = 0; $i <= 1000; $i++) {
+ echo "\n";
+for ($i = 0; $i <= 10000; $i++) {
     $total += $i;
 }
 echo $total;
+ echo "\n";
 $fruits = array("grape", "peach", "cherry", "strawberry", "banana");
 foreach($fruits as $fruit){
     echo "要素は" . $fruit;
@@ -27,5 +29,6 @@ for($i = $start; $i < $end; $i++){
   // 5で割り切れたら{}内を実行する
   if($i % 5 == 0){
     echo $i;
+    echo PHP_EOL;
   }
 }
